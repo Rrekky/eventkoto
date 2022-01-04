9c44c5ddcbc359b0a61e16a8631f8e5d3a68e93b
Rrekky <73595759+aughost-sus@users.noreply.github.com>
2022-01-04 23:57:49 +0800

    ....

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import banner from './images/banner.png';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import CreateAcc from "./Login/CreateAcc";
 import Sidebar from "./dashboard/sidebar";
+import Dashboard from "./dashboard/dashboard"
 
 function App() {  
   return (
@@ -10,7 +11,7 @@ function App() {
       <div className="App">
         <Route exact path="/" component={LoginPage}></Route>
         <Route path="/create" component={CreateAcc}></Route>
-        <Route path="/dashboard"  component={Sidebar}></Route>
+        <Route path="/dashboard"  component={Dashboard}></Route>
       </div>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './dashboard/index.css'
 import store from './reducers/index';
 import { Provider } from "react-redux"
 
