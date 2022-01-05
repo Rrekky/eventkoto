137c37638fc90587b8484756922591e5a92d89f4
Rrekky <73595759+aughost-sus@users.noreply.github.com>
2022-01-05 17:05:13 +0800

    added image slider in dashboard

--- src/dashboard/dashboard-main.js
+++ src/dashboard/dashboard-main.js
@@ -1,37 +1,63 @@
-import banner from "../images/banner.png"
+import './index.css'
+import BtnSlider from './BtnSlider'
+import dataSlider from './dataSlider'
+import { useState } from 'react'
 
-const DashboardMain = () => {
-    return ( 
-        <div className="banner-slider">
-            <div class="banner-slides">
-            <input type="radio" name="radio-btn" id="radio1"/>
-        <input type="radio" name="radio-btn" id="radio2" />
-        <input type="radio" name="radio-btn" id="radio3"/>
-        <input type="radio" name="radio-btn" id="radio4"/>
-        {/*  */}
-        <div class="banner-slide first">
-                <img src={banner}/>
-            </div>
-            <div class="banner-slide">
-                <img src={banner}/>
-            </div>
-            <div class="banner-slide">
-                <img src={banner}/>
-            </div>
-            <div class="banner-slide">
-                <img src={banner} />
-            </div>
-        {/*  */}
-        <div class="nav-manual">
-            <label for="radio1" class="manual-btn"></label>
-            <label for="radio2" class="manual-btn"></label>
-            <label for="radio3" class="manual-btn"></label>
-            <label for="radio4" class="manual-btn"></label>
-        </div>
-      </div>
-        </div>
+export default function DashboardMain() {
 
-     );
+    const [slideIndex, setSlideIndex] = useState(1)
+
+    const nextSlide = () => {
+        if(slideIndex !== dataSlider.length){
+            setSlideIndex(slideIndex + 1)
+        } 
+        else if (slideIndex === dataSlider.length){
+            setSlideIndex(1)
+        }
+    }
+
+    const prevSlide = () => {
+        if(slideIndex !== 1){
+            setSlideIndex(slideIndex - 1)
+        }
+        else if (slideIndex === 1){
+            setSlideIndex(dataSlider.length)
+        }
+    }
+
+    const moveDot = index => {
+        setSlideIndex(index)
+    }
+
+    return (
+        <div className="dashboard">
+        <div className="title">TRENDING NOW</div>
+        <div className="container-slider">
+            
+            {dataSlider.map((obj, index) => {
+                return (
+                    <div
+                    key={obj.id}
+                    className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
+                    >
+                        <img 
+                        src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`} 
+                        />
+                    </div>
+                )
+            })}
+            <BtnSlider moveSlide={nextSlide} direction={"next"} />
+            <BtnSlider moveSlide={prevSlide} direction={"prev"}/>
+
+            <div className="container-dots">
+                {Array.from({length: 5}).map((item, index) => (
+                    <div 
+                    onClick={() => moveDot(index + 1)}
+                    className={slideIndex === index + 1 ? "dot active" : "dot"}
+                    ></div>
+                ))}
+            </div>
+        </div>
+        </div>
+    )
 }
- 
-export default DashboardMain;
